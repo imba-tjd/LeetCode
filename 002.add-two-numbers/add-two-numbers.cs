710fef6c2ae6fa8399f21595af3a28bdfbfb0908
imba-tjd <109224573@qq.com>
2019-01-25 20:19:01 +0800

    solve 2

---
+++ 002.add-two-numbers/add-two-numbers.cs
@@ -0,0 +1,52 @@
+using System;
+using Xunit;
+
+// Definition for singly-linked list.
+class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+    public static ListNode ListGetter(int value) =>
+    value == 0 ? null : new ListNode(value % 10) { next = ListGetter(value / 10) };
+}
+
+class AddTwoNumbersSolution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) => AddInternal(l1, l2, 0);
+    ListNode AddInternal(ListNode l1, ListNode l2, int carry)
+    {
+        int t = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+        return l1 == null && l2 == null && carry == 0 ? null :
+            new ListNode(t % 10) { next = AddInternal(l1?.next, l2?.next, t / 10) };
+    }
+}
+
+public class AddTwoNumbersTest
+{
+    [Fact]
+    void ListGetterTest()
+    {
+        ListNode ln = ListNode.ListGetter(123);
+        Assert.Equal(3, ln.val);
+        Assert.Equal(2, ln.next.val);
+        Assert.Equal(1, ln.next.next.val);
+        Assert.Null(ln.next.next.next);
+    }
+    [Theory]
+    [InlineData(1, 2, new[] { 3 })]
+    [InlineData(342, 465, new[] { 7, 0, 8 })]
+    [InlineData(3, 9, new[] { 2, 1 })]
+    void Test(int a, int b, int[] expects)
+    {
+        ListNode l1 = ListNode.ListGetter(a);
+        ListNode l2 = ListNode.ListGetter(b);
+        ListNode r = new AddTwoNumbersSolution().AddTwoNumbers(l1, l2);
+        foreach (int n in expects)
+        {
+            Assert.Equal(n, r.val);
+            r = r.next;
+        }
+        Assert.Null(r);
+    }
+}
