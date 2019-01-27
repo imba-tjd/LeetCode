b7f0bea509fe181f2eb9d190a3e4de75f9f635d2
imba-tjd <109224573@qq.com>
2019-01-27 17:17:53 +0800

    solve 3

---
+++ 003.longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -0,0 +1,65 @@
+using System;
+using Xunit;
+
+class LongestSubstringWithoutRepeatingCharactersSolution
+{
+    int[] dic = new int[127];
+    public int LengthOfLongestSubstring1(string s)
+    {
+        int from = 0, to = 0, longest = 0;
+        while (to < s.Length)
+        {
+            char key = s[to];
+            if (dic[key] == 0)
+                dic[key] = ++to; // index + 1
+            else
+            {
+                int length = to - from;
+                if (longest < length)
+                    longest = length;
+                while (from < dic[key])
+                    dic[s[from++]] = 0;
+            }
+        }
+        return Math.Max(longest, to - from);
+    }
+
+    // Not My Solution
+    public int LengthOfLongestSubstring2(string s)
+    {
+        int n = s.Length, ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++)
+        {
+            //  Console.WriteLine("");
+            //  Console.Write(i);
+            //  Console.Write(index[s[j]]);
+            i = Math.Max(index[s[j]], i);
+            //Console.Write(ans);
+            ans = Math.Max(ans, j - i + 1);
+            index[s[j]] = j + 1;
+        }
+        return ans;
+    }
+}
+public class LongestSubstringWithoutRepeatingCharactersTest
+{
+    [Theory]
+    [InlineData("abcabcbb", 3)]
+    [InlineData("bbbbb", 1)]
+    [InlineData("pwwkew", 3)]
+    [InlineData("au", 2)]
+    [InlineData("dvdf", 3)]
+    [InlineData("abba", 2)]
+    [InlineData(" ", 1), InlineData("", 0)]
+    void Test(string s, int length)
+    {
+        var solution = new LongestSubstringWithoutRepeatingCharactersSolution();
+        int result1 = solution.LengthOfLongestSubstring1(s);
+        Assert.Equal(length, result1);
+
+        int result2 = solution.LengthOfLongestSubstring2(s);
+        Assert.Equal(length, result2);
+    }
+}
