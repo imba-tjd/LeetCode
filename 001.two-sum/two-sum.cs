d074a628c35988a38deb84ef7aa82230bb9ca7f5
imba-tjd <109224573@qq.com>
2019-01-28 10:43:35 +0800

    Use namespace

--- 001.two-sum/two-sum.cs
+++ 001.two-sum/two-sum.cs
@@ -2,31 +2,34 @@ using System;
 using System.Collections.Generic;
 using Xunit;
 
-class TwoSumSolution
+namespace TwoSum
 {
-    public int[] TwoSum(int[] nums, int target)
+    class Solution
     {
-        var dic = new Dictionary<int, int>(nums.Length);
-        for (int i = 0; i < nums.Length; i++)
-            if (!dic.ContainsKey(target - nums[i]))
-                dic.TryAdd(nums[i], i);
-            else
-                return new[] { dic[target - nums[i]], i };
-        return null;
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, int>(nums.Length);
+            for (int i = 0; i < nums.Length; i++)
+                if (!dic.ContainsKey(target - nums[i]))
+                    dic.TryAdd(nums[i], i);
+                else
+                    return new[] { dic[target - nums[i]], i };
+            return null;
+        }
+    }
+
+    public class UnitTest
+    {
+        [Theory]
+        [InlineData(new[] { 2, 7, 11, 15 }, 9, 0, 1)]
+        [InlineData(new[] { 1, 1, 2, 3 }, 5, 2, 3)]
+        void Test(int[] nums, int target, int expect1, int expect2)
+        {
+            var solution = new Solution();
+            int[] result = solution.TwoSum(nums, target);
+            Assert.Equal(expect1, result[0]);
+            Assert.Equal(expect2, result[1]);
+        }
     }
 }
 
-public class TwoSumTest
-{
-    TwoSumSolution twoSum = new TwoSumSolution();
-
-    [Theory]
-    [InlineData(new[] { 2, 7, 11, 15 }, 9, 0, 1)]
-    [InlineData(new[] { 1, 1, 2, 3 }, 5, 2, 3)]
-    void Test(int[] nums, int target, int expect1, int expect2)
-    {
-        int[] result = twoSum.TwoSum(nums, target);
-        Assert.Equal(expect1, result[0]);
-        Assert.Equal(expect2, result[1]);
-    }
-}
--- 002.add-two-numbers/add-two-numbers.cs
+++ 002.add-two-numbers/add-two-numbers.cs
@@ -1,52 +1,56 @@
 using System;
 using Xunit;
 
-// Definition for singly-linked list.
-class ListNode
+namespace AddTwoNumbers
 {
-    public int val;
-    public ListNode next;
-    public ListNode(int x) { val = x; }
-    public static ListNode ListGetter(int value) =>
-    value == 0 ? null : new ListNode(value % 10) { next = ListGetter(value / 10) };
-}
-
-class AddTwoNumbersSolution
-{
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) => AddInternal(l1, l2, 0);
-    ListNode AddInternal(ListNode l1, ListNode l2, int carry)
+    class ListNode // Definition for singly-linked list.
     {
-        int t = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
-        return l1 == null && l2 == null && carry == 0 ? null :
-            new ListNode(t % 10) { next = AddInternal(l1?.next, l2?.next, t / 10) };
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+        public static ListNode ListGetter(int value) =>
+        value == 0 ? null : new ListNode(value % 10) { next = ListGetter(value / 10) };
     }
-}
 
-public class AddTwoNumbersTest
-{
-    [Fact]
-    void ListGetterTest()
+    class Solution
     {
-        ListNode ln = ListNode.ListGetter(123);
-        Assert.Equal(3, ln.val);
-        Assert.Equal(2, ln.next.val);
-        Assert.Equal(1, ln.next.next.val);
-        Assert.Null(ln.next.next.next);
-    }
-    [Theory]
-    [InlineData(1, 2, new[] { 3 })]
-    [InlineData(342, 465, new[] { 7, 0, 8 })]
-    [InlineData(3, 9, new[] { 2, 1 })]
-    void Test(int a, int b, int[] expects)
-    {
-        ListNode l1 = ListNode.ListGetter(a);
-        ListNode l2 = ListNode.ListGetter(b);
-        ListNode r = new AddTwoNumbersSolution().AddTwoNumbers(l1, l2);
-        foreach (int n in expects)
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2) => AddInternal(l1, l2, 0);
+        ListNode AddInternal(ListNode l1, ListNode l2, int carry)
         {
-            Assert.Equal(n, r.val);
-            r = r.next;
+            int t = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+            return l1 == null && l2 == null && carry == 0 ? null :
+                new ListNode(t % 10) { next = AddInternal(l1?.next, l2?.next, t / 10) };
         }
-        Assert.Null(r);
+    }
+
+    public class UnitTest
+    {
+        [Fact]
+        void ListGetterTest()
+        {
+            ListNode ln = ListNode.ListGetter(123);
+            Assert.Equal(3, ln.val);
+            Assert.Equal(2, ln.next.val);
+            Assert.Equal(1, ln.next.next.val);
+            Assert.Null(ln.next.next.next);
+        }
+
+        [Theory]
+        [InlineData(1, 2, new[] { 3 })]
+        [InlineData(342, 465, new[] { 7, 0, 8 })]
+        [InlineData(3, 9, new[] { 2, 1 })]
+        void Test(int a, int b, int[] expects)
+        {
+            ListNode l1 = ListNode.ListGetter(a);
+            ListNode l2 = ListNode.ListGetter(b);
+            var solution = new Solution();
+            ListNode r = solution.AddTwoNumbers(l1, l2);
+            foreach (int n in expects)
+            {
+                Assert.Equal(n, r.val);
+                r = r.next;
+            }
+            Assert.Null(r);
+        }
     }
 }
