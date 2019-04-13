3f6a543cf8a78a02d5fe6a599241da0a1ef08506
imba-tjd <109224573@qq.com>
2019-04-13 10:31:22 +0800

    Solve 8

---
+++ 008.string-to-integer-atoi/Readme.md
@@ -0,0 +1,13 @@
+# [8. String to Integer (atoi)](https://leetcode.com/problems/string-to-integer-atoi/)
+
+Implement atoi which converts a string to an integer.
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: `[−231,  231 − 1]`. If the numerical value is out of the range of representable values, INT_MAX (2^31 − 1) or INT_MIN (−2^31) is returned.
+
+## Remarks
+
+要先计算ch-'0'在与前面的加起来，否则如果先加ch可能就溢出了。
---
+++ 008.string-to-integer-atoi/string-to-integer-atoi.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace StringtoIntegeratoi
+{
+    public interface ISolution { int MyAtoi(string str); }
+    class Solution : ISolution
+    {
+        public int MyAtoi(string str)
+        {
+            var span = str.AsSpan();
+            span = span.TrimStart(' ');
+            if (span.Length == 0)
+                return 0;
+            bool negative = false;
+            int position = 0;
+            if (span[position] == '-')
+            {
+                negative = true;
+                position++;
+            }
+            else if (span[position] == '+')
+                position++;
+            int value = 0;
+            while (position < span.Length && char.IsDigit(span[position]))
+                try
+                {
+                    checked { value = value * 10 + (span[position] - '0'); }
+                    position++;
+                }
+                catch
+                {
+                    return negative ? int.MinValue : int.MaxValue;
+                }
+            return negative ? -value : value;
+        }
+    }
+
+    // abstract
+    public class MultiTest
+    {
+        protected virtual ISolution GetSo => new Solution();
+
+        [Theory]
+        [
+        // InlineData("42", 42), InlineData("   -42", -42), InlineData("4193 with words", 4193),
+        // InlineData("words and 987", 0), InlineData("-91283472332", -2147483648), InlineData("", 0),
+        InlineData("2147483646", 2147483646)
+        ]
+        public void Test(string input, int expect)
+        {
+            var so = GetSo;
+            var result = so.MyAtoi(input);
+            Assert.Equal(expect, result);
+        }
+    }
+    // public class Test1 : MultiTest { override protected ISolution GetSo => new Solution(); }
+    // public class Test2 : MultiTest { override protected ISolution GetSo => new Solution2(); }
+}
