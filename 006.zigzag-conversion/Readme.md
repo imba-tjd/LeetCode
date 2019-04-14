044e13a3113660eee2038a470e24fc4f3757ba34
imba-tjd <109224573@qq.com>
2019-04-14 19:57:53 +0800

    Solve 6

---
+++ 006.zigzag-conversion/Readme.md
@@ -0,0 +1,33 @@
+# [6. ZigZag Conversion](https://leetcode.com/problems/zigzag-conversion/)
+
+Input: s = "PAYPALISHIRING", numRows = 4<br>
+Output: "PINALSIGYAHRPI"
+
+Explanation:
+
+```
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+```
+
+## Remarks
+
+### 方法一
+
+最容易想到的是建立row个string，每次取一个字符放进去，用一个变量控制向下移动还是向上移动。
+
+因为当row为1时，第0行既是上边界也是下边界，如果直接向下移动就会超出去，所以直接返回s算了。
+
+### 方法二
+
+直接按输出的顺序计算，外围循环是numRows，而内部是计算k。
+
+对于第0行，每个字符是`k*(numRows*2-2)`；对于第row行，则是`k*(numRows*2-2)+row`（对第0行也适用）与`(k+1)*(numRows*2-2)-row`（即第0行的下一个往回数）。
+
+但最后一行时也只有一个且第一个数对第0行也适用，所以后者的条件还要加上row!=0&&row!=numRows-1。
+
+另外还需考虑组不全时数字不存在的情况，非第0行的两个数都可能发生。
+
+因为当row为1时也要单独写出来，不可省略。
---
+++ 006.zigzag-conversion/zigzag-conversion.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Text;
+using Xunit;
+
+namespace ZigZagConversion
+{
+    public interface ISolution { string Convert(string s, int numRows); }
+    class Solution : ISolution
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1)
+                return s;
+            StringBuilder[] sbl = new StringBuilder[numRows];
+            for (int j = 0; j < sbl.Length; j++)
+                sbl[j] = new StringBuilder();
+            numRows -= 1;
+            int row = 0;
+            bool inc = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                sbl[row].Append(s[i]);
+                if (row == 0)
+                    inc = true;
+                else if (row == numRows)
+                    inc = false;
+                if (inc)
+                    row++;
+                else
+                    row--;
+            }
+            return string.Concat(sbl as object[]);
+        }
+    }
+    class Solution2 : ISolution
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1) // 不可省略
+                return s;
+            StringBuilder sb = new StringBuilder(s.Length);
+            int cycleLen = 2 * numRows - 2;
+            for (int row = 0; row < numRows; row++)
+                for (int j = 0; j + row < s.Length; j += cycleLen) // j+row即第一个数字的位置
+                {
+                    sb.Append(s[j + row]);
+                    int second = j + cycleLen - row;
+                    if (row != 0 && row != numRows - 1 && second < s.Length)
+                        sb.Append(s[second]);
+                }
+
+            return sb.ToString();
+        }
+    }
+
+    abstract
+    public class MultiTest
+    {
+        protected virtual ISolution GetSo => new Solution2();
+
+        [Theory]
+        [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"), InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI"),
+        InlineData("AB", 1, "AB"), InlineData("", 0, "")]
+        public void Test(string input, int numRows, string expect)
+        {
+            var so = GetSo;
+            var result = so.Convert(input, numRows);
+            Assert.Equal(expect, result);
+        }
+    }
+    public class Test1 : MultiTest { override protected ISolution GetSo => new Solution(); }
+    public class Test2 : MultiTest { override protected ISolution GetSo => new Solution2(); }
+}
