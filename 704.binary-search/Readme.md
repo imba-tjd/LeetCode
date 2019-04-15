d0948ba7e5f882364eadc99b8b721e3bcf29d6d8
imba-tjd <109224573@qq.com>
2019-04-15 23:20:45 +0800

    Solve 704

---
+++ 704.binary-search/Readme.md
@@ -0,0 +1,11 @@
+# [704. Binary Search](https://leetcode.com/problems/binary-search/)
+
+Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+
+## Remarks
+
+重点在于正常的返回需要控制在循环内，而循环外直接返回-1就好。
+
+当使用左闭右开区间时，循环条件为to > from，则循环内to至少比from大1，最少时即只有from这一个元素，此时from==midi。移动时如果mid不匹配，则midi直接赋给to，或+1后赋给from。
+
+使用闭区间时，循环条件为to >= from，即循环内to和from可相等，此时只有一个元素，midi==from==to。如果循环条件用to>from，则to==from时跳出，在最后需要判断当前值是否等于target；这样又会引入数组为空时的特殊情况，此问题前面的方法没有因为最后一步时无需访问数组。
---
+++ 704.binary-search/binary-search.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BinarySearch
+{
+    public interface ISolution { int Search(int[] nums, int target); }
+    class Solution : ISolution
+    {
+        public int Search(int[] nums, int target)
+        {
+            int from = 0, to = nums.Length;
+            int midi, mid;
+
+            while (to > from) // to - from >= 1
+            {
+                midi = (to + from) / 2;
+                mid = nums[midi];
+                if (mid == target)
+                    return midi;
+                else if (mid > target)
+                    to = midi;
+                else
+                    from = midi + 1;
+            }
+            return -1;
+        }
+    }
+
+    class Solution2 : ISolution
+    {
+        public int Search(int[] nums, int target)
+        {
+            // if (nums.Length == 0)
+            //     return -1;
+
+            int from = 0, to = nums.Length - 1;
+            int midi, mid;
+
+            while (to >= from)
+            {
+                midi = (to + from) / 2;
+                mid = nums[midi];
+                if (mid == target)
+                    return midi;
+                else if (mid > target)
+                    to = midi - 1;
+                else
+                    from = midi + 1;
+            }
+            // return nums[from] == target ? from : -1;
+            return -1;
+        }
+    }
+
+    // abstract
+    public class MultiTest
+    {
+        protected virtual ISolution GetSo => new Solution2();
+
+        [Theory]
+        [
+            InlineData(new[] { -1, 0, 3, 5, 9, 12 }, 9, 4),
+        InlineData(new[] { -1, 0, 3, 5, 9, 12 }, 2, -1),
+        InlineData(new int[0], 1, -1),
+        InlineData(new[] { 1 }, 1, 0)
+        ]
+        public void Test(int[] input, int target, int expect)
+        {
+            var so = GetSo;
+            var result = so.Search(input, target);
+            Assert.Equal(expect, result);
+        }
+    }
+    // public class Test1 : MultiTest { override protected ISolution GetSo => new Solution(); }
+    // public class Test2 : MultiTest { override protected ISolution GetSo => new Solution2(); }
+}
