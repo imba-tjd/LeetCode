19a29fa024bc030fa172cf967b4eef222d6ac239
imba-tjd <109224573@qq.com>
2019-04-20 21:03:06 +0800

    Solve 58

---
+++ 058.length-of-last-word/Readme.md
@@ -0,0 +1,9 @@
+# [58. Length of Last Word](https://leetcode.com/problems/length-of-last-word/)
+
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+## Remarks
+
+踩的人很多😂，就是在`"a "`这个用例上。三种解决方法都没什么好说的。
---
+++ 058.length-of-last-word/length-of-last-word.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LengthofLastWord
+{
+    public interface ISolution { int LengthOfLastWord(string s); }
+    class Solution : ISolution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim(); // 可以用AsSpan但没必要
+            int len = 0;
+            foreach (char ch in s)
+            {
+                if (ch == ' ')
+                    len = 0;
+                else len++;
+            }
+            return len;
+        }
+    }
+
+
+    class Solution2 : ISolution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            return s.Length - s.LastIndexOf(' ') - 1;
+        }
+    }
+
+    class Solution3 : ISolution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            int len1 = 0, len2 = 0;
+            foreach (char ch in s)
+                if (ch == ' ')
+                    len2 = 0;
+                else
+                    len1 = ++len2;
+            return len1;
+        }
+    }
+
+    // abstract
+    public class MultiTest
+    {
+        protected virtual ISolution GetSo => new Solution2();
+
+        [Theory]
+        [InlineData("Hello World", 5), InlineData("asdf", 4), InlineData("  ", 0), InlineData("a ", 1)]
+        public void Test(string input, int expect)
+        {
+            var so = GetSo;
+            var result = so.LengthOfLastWord(input);
+            Assert.Equal(expect, result);
+        }
+    }
+    // public class Test1 : MultiTest { override protected ISolution GetSo => new Solution(); }
+    // public class Test2 : MultiTest { override protected ISolution GetSo => new Solution2(); }
+}
