88b69107579ac1e084c2f2d66974edf878b916be
imba-tjd <109224573@qq.com>
2019-05-06 23:28:57 +0800

    Add KMP

--- 028.implement-strstr/implement-strstr.cs
+++ 028.implement-strstr/implement-strstr.cs
@@ -11,36 +11,100 @@ namespace ImplementstrStr
         {
             for (int i = 0; i + needle.Length <= haystack.Length; i++)
             {
+                // bool ok = true;
                 int j = -1;
                 while (++j < needle.Length)
                     if (haystack[i + j] != needle[j])
-                        break;
-                if (j == needle.Length)
+                        break; // ok = false;
+                if (j == needle.Length)  // if (ok)
                     return i;
-
-                // bool ok = true;
-                // for (int j = 0; j < needle.Length; j++)
-                //     if (haystack[i + j] != needle[j])
-                //     {
-                //         ok = false;
-                //         break;
-                //     }
-                // if (ok)
-                //     return i;
             }
             return -1;
         }
     }
 
-    abstract
+    class Solution2 : ISolution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Length == 0)
+                return 0;
+            int[] next = GetNext(needle);
+            int i = 0, j = 0;
+
+            while (i < haystack.Length && j < needle.Length)
+                if (j == -1 || haystack[i] == needle[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                    j = next[j];
+
+            return j == needle.Length ? i - j : -1;
+        }
+
+        public int[] GetNext(string p)
+        {
+            int[] next = new int[p.Length];
+            next[0] = -1;
+            int j = 0, k = -1;
+
+            while (j < p.Length - 1)
+                if (k == -1 || p[j] == p[k])
+                    next[++j] = ++k;
+                else
+                    k = next[k];
+            return next;
+        }
+    }
+
+    // 两个方法都是错的
+    class Solution3 : ISolution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Length == 0)
+                return 0;
+            int[] next = GetNext(needle);
+
+            int j = 0;
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (j == needle.Length)
+                    return i - j;
+                else if (haystack[i] == needle[j])
+                    j++;
+                else if (j != 0)
+                {
+                    j = next[j];
+                    i--;
+                }
+            }
+            return j == needle.Length ? 0 : -1;
+        }
+
+        public int[] GetNext(string pattern)
+        {
+            int[] next = new int[pattern.Length];
+            next[0] = -1;
+
+            for (int j = 0; j < pattern.Length - 1; j++)
+                next[j + 1] = next[j] == -1 || pattern[next[j]] == pattern[j] ? next[j] + 1 : 0;
+
+            return next;
+        }
+    }
+
+    // abstract
     public class MultiTest
     {
-        protected virtual ISolution GetSo => new Solution();
+        protected virtual ISolution GetSo => new Solution2();
 
         [Theory]
         [InlineData("hello", "ll", 2), InlineData("aaaaa", "bba", -1), InlineData("asdf", "asdf", 0),
-        InlineData("", "a", -1), InlineData("a", "", 0), InlineData("", "", 0)
-        ]
+        InlineData("", "a", -1), InlineData("a", "", 0), InlineData("", "", 0),
+        InlineData("mississippi", "issip", 4), InlineData("mississippi", "pi", 9), InlineData("aabaaabaaac", "aabaaac", 4)]
         public void Test(string input, string needle, int expect)
         {
             var so = GetSo;
@@ -48,6 +112,20 @@ namespace ImplementstrStr
             Assert.Equal(expect, result);
         }
     }
-    public class Test1 : MultiTest { override protected ISolution GetSo => new Solution(); }
+    // public class Test1 : MultiTest { override protected ISolution GetSo => new Solution(); }
     // public class Test2 : MultiTest { override protected ISolution GetSo => new Solution2(); }
+
+    public class GetNextTest
+    {
+        [Theory]
+        [InlineData("abababcab", new[] { -1, 0, 0, 1, 2, 3, 4, 0, 1 }), InlineData("a", new[] { -1 }),
+        InlineData("issip", new[] { -1, 0, 0, 0, 1 }), InlineData("aabaaac", new[] { -1, 0, 1, 0, 1, 2, 2 })]
+        public void Test(string p, int[] expect)
+        {
+            int[] result = new Solution2().GetNext(p);
+
+            Assert.Equal(p.Length, result.Length);
+            Assert.Equal(expect, result);
+        }
+    }
 }
