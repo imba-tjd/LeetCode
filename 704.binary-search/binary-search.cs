cdbca17e7946109d97976c5e1be824281b81271b
imba-tjd <109224573@qq.com>
2019-10-06 17:27:02 +0800

    Add recursive solution

--- 704.binary-search/binary-search.cs
+++ 704.binary-search/binary-search.cs
@@ -7,7 +7,7 @@ namespace BinarySearch
     public interface ISolution { int Search(int[] nums, int target); }
     class Solution : ISolution
     {
-        public int Search(int[] nums, int target)
+        public int Search(int[] nums, int target) // 左开右闭区间
         {
             int from = 0, to = nums.Length;
             int midi, mid;
@@ -35,7 +35,7 @@ namespace BinarySearch
             // if (nums.Length == 0)
             //     return -1;
 
-            int from = 0, to = nums.Length - 1;
+            int from = 0, to = nums.Length - 1; // 闭区间
             int midi, mid;
 
             while (to >= from)
@@ -52,6 +52,32 @@ namespace BinarySearch
             }
             // return nums[from] == target ? from : -1;
             return -1;
+        }
+    }
+
+    class Solution3 : ISolution
+    {
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length == 0)
+                return -1;
+            return BSInternal(nums, 0, nums.Length, target);
+        }
+
+        // 递归解法，而且测试速度比上面两个都快一点
+        int BSInternal(int[] nums, int from, int to, int target)
+        {
+            if (to - from == 1)
+                return nums[from] == target ? from : -1;
+
+            int mid = from + (to - from) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                return BSInternal(nums, mid, to, target);
+            else
+                return BSInternal(nums, from, mid, target);
         }
     }
 
@@ -72,4 +98,5 @@ namespace BinarySearch
     }
     public class Test1 : MultiTest { override protected ISolution GetSo => new Solution(); }
     public class Test2 : MultiTest { override protected ISolution GetSo => new Solution2(); }
+    public class Test3 : MultiTest { override protected ISolution GetSo => new Solution3(); }
 }
