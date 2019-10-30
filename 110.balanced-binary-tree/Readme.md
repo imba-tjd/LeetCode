64bee6537b45eaa27483037da55b46e03d28a7c9
imba-tjd <109224573@qq.com>
2019-10-30 08:24:02 +0800

    solve 110

---
+++ 110.balanced-binary-tree/Readme.md
@@ -0,0 +1,9 @@
+# [110. Balanced Binary Tree](https://leetcode.com/problems/balanced-binary-tree/)
+
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+> a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+## Remarks
---
+++ 110.balanced-binary-tree/balanced-binary-tree.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using LCDS;
+
+namespace BalancedBinaryTree
+{
+    public interface ISolution { bool IsBalanced(TreeNode root); }
+    class Solution : ISolution
+    {
+        public bool IsBalanced(TreeNode root) => GetDepth(root) != -1; // 使用-1来判断是否平衡，不用另外的变量
+
+        int GetDepth(TreeNode tn) =>
+            tn == null ? 0 :
+            GetDepth(tn.left) is int l && l == -1 ? -1 :
+            GetDepth(tn.right) is int r && r == -1 ? -1 :
+            Math.Abs(l - r) > 1 ? -1 :
+            Math.Max(l, r) + 1;
+    }
+
+    // abstract
+    public class MultiTest
+    {
+        protected virtual ISolution GetSo => new Solution();
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new int?[] { 3, 9, 20, null, null, 15, 7 }, true };
+            yield return new object[] { new int?[] { 1, 2, 2, 3, 3, null, null, 4, 4 }, false };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int?[] treearr, bool expect)
+        {
+            var so = GetSo;
+            var tree = TreeNode.Create(treearr);
+            var result = so.IsBalanced(tree);
+            Assert.Equal(expect, result);
+        }
+    }
+    // public class Test1 : MultiTest { override protected ISolution GetSo => new Solution(); }
+    // public class Test2 : MultiTest { override protected ISolution GetSo => new Solution2(); }
+}
