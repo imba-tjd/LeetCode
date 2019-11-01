fc20ebe88b7c6b7014fb977a78820eb17639f100
imba-tjd <109224573@qq.com>
2019-11-01 21:21:40 +0800

    solve 637

---
+++ 637.average-of-levels-in-binary-tree/Readme.md
@@ -0,0 +1,17 @@
+# [637. Average of Levels in Binary Tree](https://leetcode.com/problems/average-of-levels-in-binary-tree/)
+
+Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+
+```
+    3
+   / \
+  9  20
+    /  \
+   15   7
+```
+
+Output: `[3, 14.5, 11]`
+
+Explanation: The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return `[3, 14.5, 11]`.
+
+## Remarks
---
+++ 637.average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using LCDS;
+
+namespace Problems.AverageofLevelsinBinaryTree
+{
+    public interface ISolution { IList<double> AverageOfLevels(TreeNode root); }
+    class Solution : ISolution
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            var l = new List<double>();
+            if (root == null)
+                return l;
+
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+            while (q.Count != 0)
+            {
+                int len = q.Count;
+                double total = 0;
+                for (int i = 0; i < len; i++)
+                {
+                    var c = q.Dequeue();
+                    total += c.val;
+                    if (c.left != null)
+                        q.Enqueue(c.left);
+                    if (c.right != null)
+                        q.Enqueue(c.right);
+                }
+                l.Add(total / len);
+            }
+
+            return l;
+        }
+    }
+
+    // abstract
+    public class MultiTest
+    {
+        protected virtual ISolution GetSo => new Solution();
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new int?[] { 3, 9, 20, null, null, 15, 7 }, new[] { 3, 14.5, 11 } };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int?[] treearr, double[] expect)
+        {
+            var so = GetSo;
+            var tree = TreeNode.Create(treearr);
+            var result = so.AverageOfLevels(tree);
+            Assert.Equal(expect, result);
+        }
+    }
+    // public class Test1 : MultiTest { override protected ISolution GetSo => new Solution(); }
+    // public class Test2 : MultiTest { override protected ISolution GetSo => new Solution2(); }
+}
