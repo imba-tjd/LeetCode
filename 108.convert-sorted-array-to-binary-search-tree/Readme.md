5e303c502dd9d4bcc6454b36bcdf521433f3780b
imba-tjd <109224573@qq.com>
2019-11-06 13:38:20 +0800

    solve 108

---
+++ 108.convert-sorted-array-to-binary-search-tree/Readme.md
@@ -0,0 +1,19 @@
+# [108. Convert Sorted Array to Binary Search Tree](https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/)
+
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+Input: [-10,-3,0,5,9]
+
+One possible answer is:
+
+```
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+```
+
+## Remarks
+
+这其实是最佳AVL树的算法，思想类似于二分搜索，必须用左闭右开区间。最后形成的效果是一个完全二叉树的AVL树。
---
+++ 108.convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using LCDS;
+
+namespace Problems.ConvertSortedArraytoBinarySearchTree
+{
+    public interface ISolution { TreeNode SortedArrayToBST(int[] nums); }
+    class Solution : ISolution
+    {
+        public TreeNode SortedArrayToBST(int[] nums) => SortedArrayToBST(nums, 0, nums.Length);
+        TreeNode SortedArrayToBST(int[] nums, int from, int to)
+        {
+            if (to <= from)
+                return null;
+
+            int mid = from + (to - from) / 2;
+
+            var tn = new TreeNode(nums[mid]);
+            tn.left = SortedArrayToBST(nums, from, mid);
+            tn.right = SortedArrayToBST(nums, mid + 1, to);
+
+            return tn;
+        }
+    }
+
+    class Solution2 : ISolution
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            if (nums.Length == 0)
+                return null;
+
+            var s = new Stack<(TreeNode, int, int)>();
+            var root = new TreeNode(-1);
+            s.Push((root, 0, nums.Length));
+
+            while (s.Count != 0)
+            {
+                var (tn, from, to) = s.Pop();
+                int mid = from + (to - from) / 2;
+                tn.val = nums[mid];
+
+                if (from < mid)
+                    s.Push((tn.left = new TreeNode(-1), from, mid));
+                if (mid + 1 < to)
+                    s.Push((tn.right = new TreeNode(-1), mid + 1, to));
+            }
+
+            return root;
+        }
+    }
+
+    abstract
+    public class MultiTest
+    {
+        protected virtual ISolution GetSo => new Solution2();
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { new[] { -10, -3, 0, 5, 9 }, new int?[] { 0, -3, 9, -10, null, 5 } };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] arr, int?[] expect)
+        {
+            var so = GetSo;
+            var result = so.SortedArrayToBST(arr);
+            Assert.Equal(expect, result.AsEnumerable());
+        }
+    }
+    public class Test1 : MultiTest { override protected ISolution GetSo => new Solution(); }
+    public class Test2 : MultiTest { override protected ISolution GetSo => new Solution2(); }
+}
