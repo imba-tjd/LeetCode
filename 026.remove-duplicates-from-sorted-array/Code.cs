b45452269eeada48ae09930775a331ef43b2957a
imba-tjd <109224573@qq.com>
2021-11-08 21:52:38 +0800

    复习026

---
+++ 026.remove-duplicates-from-sorted-array/Code.cs
@@ -0,0 +1,52 @@
+using Xunit;
+
+namespace LeetCode.Problems.P026RemoveDuplicatesfromSortedArray
+{
+    public interface ISolution { int RemoveDuplicates(int[] nums); }
+    class Solution : ISolution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int i = 0;
+            int? pre = null;
+            for (int j = 0; j < nums.Length; j++)
+                if (pre != nums[j])
+                    pre = nums[i++] = nums[j];
+            return i;
+        }
+    }
+
+    class Solution2 : ISolution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            int i = 0;
+            for (int j = 0; j < nums.Length; j++)
+                if (nums[i] != nums[j])
+                    nums[++i] = nums[j];
+            return i + 1;
+        }
+    }
+
+    abstract
+    public class MultiTest
+    {
+        protected virtual ISolution GetSo => new Solution();
+
+        [Theory]
+        [InlineData(new[] { 1, 1, 2 }, new[] { 1, 2 })]
+        [InlineData(new int[]{}, new int[]{})]
+        public void Test(int[] input, int[] expect)
+        {
+            var so = GetSo;
+            int n = so.RemoveDuplicates(input);
+            int[] result = input[0..(n)];
+            Assert.Equal(expect, result);
+        }
+    }
+    public class Test1 : MultiTest { protected override ISolution GetSo => new Solution(); }
+    public class Test2 : MultiTest { protected override ISolution GetSo => new Solution2(); }
+}
--- 026.remove-duplicates-from-sorted-array/Readme.md
+++ 026.remove-duplicates-from-sorted-array/Readme.md
@@ -1,9 +1,13 @@
 # [26. Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/)
 
-Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+Given a sorted array nums, remove the duplicates **in-place** such that each element appear only once and return the new length.
 
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+返回修改后的数组长度。
 
 ## Remarks
 
-用int?注意赋值顺序，int到int?是可以隐式转化的，写成`nums[i++] = pre = nums[j];`就不行了。
+快慢指针：一个一直扫描，另一个指示待赋值位置。但是需要判断什么时候发生变化。
+
+一种方式是以快指针为中心，再用一个变量记录它前一个的值。此时i就是真正的待赋值位置。用int?注意赋值顺序，int到int?是可以隐式转化的，不能写成`nums[i++] = pre = nums[j];`。
+
+第二种方式是用慢指针记录当前值，当不等时先把慢指针移动到下一个位置，再赋值。因为慢指针初始值是0，隐含处理过了第一个位置，导致数组长度为0时必须单独处理。
