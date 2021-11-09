ff8857211f7bea78565254269a28ae8f00b4cbe1
imba-tjd <109224573@qq.com>
2021-11-09 13:53:33 +0800

    复习002

---
+++ 002.add-two-numbers/Code.cs
@@ -0,0 +1,36 @@
+using LeetCode.DataStructures;
+
+namespace LeetCode.Problems.P002AddTwoNumbers;
+
+class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) => AddInternal(l1, l2, 0);
+    ListNode AddInternal(ListNode l1, ListNode l2, int carry)
+    {
+        int t = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+        return l1 == null && l2 == null && carry == 0 ? null :
+            new ListNode(t % 10, AddInternal(l1?.next, l2?.next, t / 10));
+    }
+}
+
+public class UnitTest
+{
+    [Theory]
+    [InlineData(1, 2, new[] { 3 })]
+    [InlineData(342, 465, new[] { 7, 0, 8 })]
+    [InlineData(3, 9, new[] { 2, 1 })]
+    void Test(int a, int b, int[] expects)
+    {
+        ListNode l1 = ListNodeHelper.Create(a);
+        ListNode l2 = ListNodeHelper.Create(b);
+        var solution = new Solution();
+
+        ListNode r = solution.AddTwoNumbers(l1, l2);
+        foreach (int n in expects)
+        {
+            Assert.Equal(n, r.val);
+            r = r.next;
+        }
+        Assert.Null(r);
+    }
+}
--- 002.add-two-numbers/Readme.md
+++ 002.add-two-numbers/Readme.md
@@ -1,4 +1,4 @@
-# 2. Add Two Numbers
+# [2. Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)
 
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
@@ -6,4 +6,4 @@ You may assume the two numbers do not contain any leading zero, except the numbe
 
 ## Remarks
 
-可能会爆栈，不过改成迭代的并不难。
+把两个结点的值加起来，大于10就进位(carry)。
