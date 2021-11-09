8f89103714e398b7c4a81b9f0295a14bbccfbb9e
imba-tjd <109224573@qq.com>
2021-11-09 09:04:16 +0800

    复习027

---
+++ 027.remove-element/Code.cs
@@ -0,0 +1,68 @@
+namespace LeetCode.Problems.P027RemoveElement;
+
+public interface ISolution { int RemoveElement(int[] nums, int val); }
+class Solution : ISolution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        if (nums.Length == 0)
+            return 0;
+
+        int p = 0, q = nums.Length - 1;
+        while (true)
+        {
+            while (p < q && nums[p] != val)
+                p++;
+            if (p == q)
+                return nums[p] == val ? p : p + 1;
+
+            nums[p] = nums[q--];
+        }
+    }
+}
+
+class Solution2 : ISolution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int p = 0;
+        for (int i = 0; i < nums.Length; i++)
+            if (nums[i] != val)
+                nums[p++] = nums[i];
+        return p;
+    }
+}
+
+class Solution3 : ISolution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int p = 0, q = nums.Length;
+        while (p < q)
+            if (nums[p] != val)
+                p++;
+            else
+                nums[p] = nums[--q];
+        return q;
+    }
+}
+
+abstract
+public class MultiTest
+{
+    protected virtual ISolution GetSo => new Solution();
+
+    [Theory]
+    [InlineData(new[] { 3, 2, 2, 3 }, 3, 2), InlineData(new[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5),
+    InlineData(new[] { 1 }, 1, 0), InlineData(new[] { 1 }, 0, 1), InlineData(new int[0], 1, 0),
+    InlineData(new[] { 3, 3 }, 3, 0), InlineData(new[] { 1, 1, 0 }, 1, 1), InlineData(new[] { 1, 0 }, 1, 1)]
+    public void Test(int[] input, int val, int expect)
+    {
+        var so = GetSo;
+        var result = so.RemoveElement(input, val);
+        Assert.Equal(expect, result);
+    }
+}
+public class Test1 : MultiTest { protected override ISolution GetSo => new Solution(); }
+public class Test2 : MultiTest { protected override ISolution GetSo => new Solution2(); }
+public class Test3 : MultiTest { protected override ISolution GetSo => new Solution3(); }
