005f29b5e7918bfdcce0c1158bb9fd92ed7d5961
imba-tjd <109224573@qq.com>
2022-03-19 17:13:42 +0800

    复习196

--- 196.delete-duplicate-emails/code.sql
+++ 196.delete-duplicate-emails/code.sql
@@ -1,15 +1,25 @@
-delete
-from Person
-where id not in (select * from (
-    select min(id) from Person
-    group by email
+-- [196. Delete Duplicate Emails](https://leetcode.com/problems/delete-duplicate-emails/) 删除重复的电子邮箱
+-- 基本思路是找出重复的email的id，删掉。
+
+DELETE
+FROM Person
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM Person
+    GROUP BY email
+)
+
+-- MySQL不允许直接删除子查询的表，要再SELECT一遍放到FROM里
+DELETE
+FROM Person
+WHERE id NOT IN (SELECT * FROM (
+    SELECT MIN(id) FROM Person
+    GROUP BY email
 ) t);
 
--- Solution2
-with t as (
-    select min(id) from Person
-    group by email
-)
-delete
-from Person
-where id not in (select * from t);
+-- 标准答案，自连接
+DELETE p1
+FROM Person p1, Person p2
+WHERE p1.email = p2.email AND p1.id > p2.id
+
+-- TODO: 窗口函数效率很高
